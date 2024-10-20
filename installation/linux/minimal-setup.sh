d92cd6c3215f93695c41a51a02322fa54dc30feb
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-10-21 00:33:10 +0200

    tools: Wrap taskfiles for linux
    tools: Wrap taskfiles for linux

--- installation/linux/minimal-setup.sh
+++ installation/linux/minimal-setup.sh
@@ -8,6 +8,7 @@ RESET=$(tput sgr0)
 
 # Print an info message
 function print_info() {
+  echo
   echo -e "${GREEN}$1${RESET}"
 }
 
@@ -32,7 +33,6 @@ PKGS=(
 )
 
 for PKG in "${PKGS[@]}"; do
-  echo
   print_info "INSTALLING PACKAGE: $PKG"
   sudo pacman -S "$PKG" --noconfirm --needed
 done
